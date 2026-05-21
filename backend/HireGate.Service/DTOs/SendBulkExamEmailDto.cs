7940ec8742b07ec87ea5156295f7fc2d577c17a0
Mohamed Medhat <muhhamedmedhat85@gmail.com>
2026-05-21 16:02:55 +0300

    white space

--- backend/HireGate.Service/DTOs/SendBulkExamEmailDto.cs
+++ backend/HireGate.Service/DTOs/SendBulkExamEmailDto.cs
@@ -3,8 +3,6 @@ namespace HireGate.Service.DTOs;
 public class SendBulkExamEmailDto
 {
     public int ExamId { get; set; }
-    public int? FinalScore { get; set; }
-    public DateTime? StartedAt { get; set; }
-    public DateTime? SubmittedAt { get; set; }
+
     public List<int> CandidateIds { get; set; } = new();
 }
