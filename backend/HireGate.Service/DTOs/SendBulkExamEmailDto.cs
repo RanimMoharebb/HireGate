da40835583e6ae4296e94a825024974ab072e946
Mohamed Medhat <muhhamedmedhat85@gmail.com>
2026-05-21 16:02:02 +0300

    allow admin to send another exam after submitting

--- backend/HireGate.Service/DTOs/SendBulkExamEmailDto.cs
+++ backend/HireGate.Service/DTOs/SendBulkExamEmailDto.cs
@@ -3,5 +3,8 @@ namespace HireGate.Service.DTOs;
 public class SendBulkExamEmailDto
 {
     public int ExamId { get; set; }
+    public int? FinalScore { get; set; }
+    public DateTime? StartedAt { get; set; }
+    public DateTime? SubmittedAt { get; set; }
     public List<int> CandidateIds { get; set; } = new();
 }
